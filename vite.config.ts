66a7137385dd2911f4dc46f12248d3426d75f898
Jae Han Song <rfv1479@gmail.com>
2023-06-23 17:34:55 +0900

    NM-12 chore : 배포를 위한 환경 세팅 및 aws 명령어 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import federation from '@originjs/vite-plugin-federation';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PREFIX_ENVIRONMENT_URL =
+  process.env.NODE_ENV === 'production' ? `${process.env.SERVER_IP}` : 'localhost';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +17,8 @@ export default defineConfig({
     federation({
       name: 'app',
       remotes: {
-        Auth: 'http://localhost:5001/assets/remoteEntry.js',
-        Books: 'http://localhost:5002/assets/remoteEntry.js',
+        Auth: `http://${PREFIX_ENVIRONMENT_URL}:5001/assets/remoteEntry.js`,
+        Books: `http://${PREFIX_ENVIRONMENT_URL}:5002/assets/remoteEntry.js`,
       },
       shared: ['react', 'react-dom'],
     }),
