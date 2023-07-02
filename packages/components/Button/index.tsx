886720beb1bc3fce208fc6e80f306d4c6b3e0c7f
Jae Han Song <rfv1479@gmail.com>
2023-07-03 02:30:21 +0900

    NM-27 fix : package tailwindCSS impoort 이슈 해결
    - 독단적으로 module을 형성하는 button이라면 CSS 파일 따로 필요
    - `npx tailwindCSS --watch` 문법을 활용하여 강제로 CSS 파일을 만들고
      따로 import

--- packages/components/Button/index.tsx
+++ packages/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React, { ButtonHTMLAttributes } from 'react';
 import { Icon, IconifyIcon } from '@iconify/react';
 
-import style from './style.module.scss';
+import '@packages/styles/tailwind-LEGACY.css';
 
 const BtnHeirarchyMap = {
   first: 'bg-brown100 text-white',
@@ -14,19 +14,20 @@ const BtnHeirarchyMap = {
 interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   iconSrc?: string | IconifyIcon;
   heirarchy?: keyof typeof BtnHeirarchyMap;
+  customClass?: string;
 }
 
 type ButtonType = (props: IButton) => React.ReactElement;
 
 export const Button: ButtonType = (props) => {
-  const { disabled, value, type = 'button', iconSrc, heirarchy = 'first' } = props;
+  const { disabled, value, type = 'button', iconSrc, heirarchy = 'first', customClass } = props;
 
   const common = 'w-full font-semibold px-10 py-3 shadow-md rounded-md';
-  const btnClass = `${common} ${BtnHeirarchyMap[heirarchy]}`;
+  const btnClass = `${common} ${customClass ? `${customClass}` : `${BtnHeirarchyMap[heirarchy]}`}`;
 
   return (
     <button className={btnClass} type={type} disabled={disabled}>
-      <div className={style.button}>
+      <div className="flex items-center justify-center gap-4">
         {iconSrc && <Icon icon={iconSrc} width={24} height={24} />}
         <p>{value}</p>
       </div>
