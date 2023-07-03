b7a5ddc6cdfc078f3dc9667e45cdc66b1a4088db
Jae Han Song <rfv1479@gmail.com>
2023-07-03 11:28:22 +0900

    NM-17 feat : 버튼 로그인 CSS 추가

--- microServices/Auth/src/components/loginArea/index.tsx
+++ microServices/Auth/src/components/loginArea/index.tsx
@@ -5,6 +5,7 @@ export default function LoginArea() {
     <section className="flex flex-col w-full gap-4">
       <Button
         onClick={() => {}}
+        disabled={true}
         customClass={'bg-white border-2 border-gray100'}
         iconSrc={'flat-color-icons:google'}
         value={'구글 계정으로 로그인'}
@@ -17,6 +18,7 @@ export default function LoginArea() {
       />
       <Button
         onClick={() => {}}
+        disabled={true}
         customClass={'bg-black100 text-white'}
         iconSrc={'mdi:apple'}
         iconColor={'#fefefe'}
