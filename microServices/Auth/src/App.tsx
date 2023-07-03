a31343d68df51d7a76e803b72f93bddd141c76e1
Jae Han Song <rfv1479@gmail.com>
2023-07-03 10:48:40 +0900

    NM-17 feat : development 때 tailwindCSS generate
    - packages에서 읽을 수 있도록 매번 generate 해주어야 함

--- microServices/Auth/src/App.tsx
+++ microServices/Auth/src/App.tsx
@@ -1,15 +1,13 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 
-import LoginMock from './mocks/account/login';
+import LoginArea from './components/LoginArea';
 
 function App() {
   const location = useLocation();
 
   return (
     <Routes location={location}>
-      <Route path="account">
-        <Route element={<LoginMock />} path="login" />
-      </Route>
+      <Route element={<LoginArea />} path="loginarea" />
       <Route element={<>페이지를 찾을 수 없습니다.</>} path="*" />
     </Routes>
   );
