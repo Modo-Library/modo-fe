1319338b9c5c29443214466403ab5c0e937cd4f5
Jae Han Song <rfv1479@gmail.com>
2023-07-13 11:39:00 +0900

    INITIATE APP

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+const reqURL =
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : 'http://host.modolib.site';
+
+export default function Home() {
+  return (
+    <main className="flex justify-around w-screen mx-auto items-center max-w-[1024px]">
+      <iframe
+        src={`${reqURL}/account/login`}
+        title="MODO Library Service"
+        className="mobile-layout"
+      />
+      <div className="items-center justify-center flex-col gap-3 flex">
+        <h1>MODO</h1>
+        <h2>모두의 도서관</h2>
+      </div>
+    </main>
+  );
+}
