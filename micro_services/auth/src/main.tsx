a7d664e3ea577516c374a8fab30733cd99f34fd7
Jae Han Song <rfv1479@gmail.com>
2023-07-14 17:30:47 +0900

    NM-53 test : jest 환경 구축
    - babel, jest, testing-library, msw 설치

--- micro_services/auth/src/main.tsx
+++ micro_services/auth/src/main.tsx
@@ -4,13 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { server } from './mock/browser';
 import App from './App';
 
 import '@packages/styles/_mobile.css';
 
-const queryClient = new QueryClient();
+server.start({ onUnhandledRequest: 'bypass' });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient();
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(container!);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen />
