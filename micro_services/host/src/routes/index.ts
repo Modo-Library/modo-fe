2dbb3a518e62feecaab1ee096390db7bba3fe4e4
Jae Han Song <79848632+Wisesaturn@users.noreply.github.com>
2023-08-05 20:39:36 +0900

    Revert "Hotfix : books 싱크 고려"

--- micro_services/host/src/routes/index.ts
+++ micro_services/host/src/routes/index.ts
@@ -7,7 +7,3 @@ export const Home = lazy(() => import('host/pages/home'));
 
 /* Account */
 export const Login = lazy(() => import('host/pages/account/login'));
-export const Profile = lazy(() => import('host/pages/profile'));
-
-/* Chat */
-export const Chat = lazy(() => import('host/pages/chat'));
