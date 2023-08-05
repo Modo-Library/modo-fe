2ec254d549661f432fff2195933c7ebce9139830
Jae Han Song <rfv1479@gmail.com>
2023-08-05 20:49:11 +0900

    NM-69 Hotfix : 로그인페이지 Navigation 노출 수정

--- micro_services/host/src/components/Layout/index.tsx
+++ micro_services/host/src/components/Layout/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useRecoilValue } from 'recoil';
+import { authSelector } from 'auth/utils/recoil/auth';
 
 import Navigation from 'host/components/Navigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const authState = useRecoilValue(authSelector);
+  const isAuth = process.env.NODE_ENV === 'production' ? authState : 'user';
+
   return (
     <>
       {children}
-      <Navigation />
+      {isAuth === 'user' && <Navigation />}
     </>
   );
 }
