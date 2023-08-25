1f32a40bbeed888efde01cac39c3720c7fbdbb90
Jae Han Song <rfv1479@gmail.com>
2023-08-25 16:54:07 +0900

    hotfix : sentry Token Hide

--- micro_services/host/src/main.tsx
+++ micro_services/host/src/main.tsx
@@ -11,10 +11,7 @@ import GlobalErrorBoundary from 'host/utils/error/globalBoundary';
 import App from './App';
 
 Sentry.init({
-  dsn:
-    process.env.NODE_ENV === 'production'
-      ? 'https://bfd353336ec70a62a21f48c2c146e1f1@o4505763747921920.ingest.sentry.io/4505763752181760'
-      : '',
+  dsn: process.env.NODE_ENV === 'production' ? process.env.VITE_SENTRY_DSN : '',
   normalizeDepth: 5,
   integrations: [new Sentry.BrowserTracing()],
   environment: process.env.NODE_ENV,
