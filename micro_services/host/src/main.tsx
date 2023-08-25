acd55e869efafd327a164b12f571f22d745f81bf
Jae Han Song <rfv1479@gmail.com>
2023-08-25 16:18:36 +0900

    feat : Sentry Host 모듈에 적용

--- micro_services/host/src/main.tsx
+++ micro_services/host/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
+import * as Sentry from '@sentry/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import '@packages/styles/_mobile.css';
@@ -8,6 +9,17 @@ import '@packages/styles/_mobile.css';
 import GlobalErrorBoundary from 'host/utils/error/globalBoundary';
 
 import App from './App';
+
+Sentry.init({
+  dsn:
+    process.env.NODE_ENV === 'production'
+      ? 'https://bfd353336ec70a62a21f48c2c146e1f1@o4505763747921920.ingest.sentry.io/4505763752181760'
+      : '',
+  normalizeDepth: 5,
+  integrations: [new Sentry.BrowserTracing()],
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 0.4,
+});
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container!);
