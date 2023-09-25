ad5033fa9eadb23ca9560e71e6aadacf2b28dec6
Jae Han Song <rfv1479@gmail.com>
2023-09-25 14:17:10 +0900

    NM-144 fix : Navigation 오류 수정 및 babel.config.js 추가

--- micro_services/auth/babel.config.js
+++ micro_services/auth/babel.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   presets: [
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-react', { runtime: 'automatic', importSource: '@emotion/react' }],
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
   ],
--- micro_services/host/src/components/Navigation/hooks/useShowNavigation.ts
+++ micro_services/host/src/components/Navigation/hooks/useShowNavigation.ts
@@ -1,17 +1,20 @@
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { authInfoSelector } from 'auth/utils/recoil/auth';
 
 const canShowNavigation = ['chat', 'profile'];
 
 export default function useShowNavigation() {
   const location = useLocation();
   const [showNavigation, setShowNavigation] = useState(false);
+  const auth = useRecoilValue(authInfoSelector);
+  const isUser = auth.usersId !== '' || process.env.NODE_ENV === 'development';
 
   useEffect(() => {
     const { pathname } = location;
-    const startsWithValidPath =
-      canShowNavigation.some((item) => pathname.startsWith(`/${item}`)) || pathname === '/';
-    if (startsWithValidPath) {
+    const startsWithValidPath = canShowNavigation.some((item) => pathname.startsWith(`/${item}`));
+    if (startsWithValidPath && isUser) {
       setShowNavigation(true);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
